8caaaaaf004cff104005f39d8106c5042238b955
AndrewKizito <AndrewKizito54@gmail.com>
2023-02-13 11:00:20 +0300

    Added Amplify App

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as amplify from "aws-cdk-lib/aws-amplify";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import path = require("path");
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /*-----------------------------------------------------------------*/
+    /*--------------------------Amplify Setup--------------------------*/
+    /*-----------------------------------------------------------------*/
+    const feroxAmplifyApp = new amplify.CfnApp(this, "Ferox-Amplify-App", {
+      name: "Ferox-Investment",
+    });
   }
 }
